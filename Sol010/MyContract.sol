821a2da6a6153b74ec4907b9206461d76d2bae3b
Dorian.H Mekni <54440936+MDH-Lab@users.noreply.github.com>
2020-11-01 15:59:32 +0100

    Update MyContract.sol

--- Sol010/MyContract.sol
+++ Sol010/MyContract.sol
@@ -1,5 +1,19 @@
-pragma solidity ^0.5.11; 
+pragma solidity ^0.5.13;
 
 contract MyContract {
-    string public myString = "Hello Blockchain !";
-} 
+    address owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function getCreator() public view returns(address) {
+        return owner;
+    }
+
+    function kill() public {
+        if(msg.sender == owner) {
+            selfdestruct(msg.sender);
+        }
+    }
+}
